8939c52d70bc7df070fee959420d2de0ef8b3e26
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2025-09-04 15:28:21 +0800

    Update buyMenu.kt

--- prog2/buyMenu.kt
+++ prog2/buyMenu.kt
@@ -29,7 +29,7 @@ fun editCart(cart: MutableList<BuyItem>) {
     // if cart not empty
 }
 
-fun checkOutMenu() {
+fun checkOutMenu(cart) {
     // ask the date
     // decrement the quantity
     // if quantity == 0, remove from list
@@ -37,7 +37,24 @@ fun checkOutMenu() {
 }
 
 fun buyMenu(user: Int) {
-    // array of BuyItem called cart, holds 10 at most
+    
     val cart = mutableListOf<BuyItem>()
-    println(addToCart(cart))
+
+    println("Press V to view all products.\nPress S to show only the products of a specific seller.\nPress C to search for products by category.\nPress N to search for products by name.\n")
+    println("Press A to add a product to your cart.\nPress E to edit your cart.\nPress O to check-out.\n")
+    println("Press X to exit without checking-out")
+
+    while (true){
+                when (readlnOrNull()?.lowercase()) {
+                "v" -> viewAllProducts()
+                "s" -> showProductsofSpecific()
+                "c" -> searchByCategory()
+                "n" -> searchByName()
+                "a" -> addToCart(cart)
+                "e" -> editCart(cart)
+                "o" -> checkOutMenu(cart)
+                "x" -> return
+                else -> println("\tInvalid input. Please try again:\n")
+            }
+        }
 }
